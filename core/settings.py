f337bc58c5558cb93d363eb660fa81ee2f8ee07f
CRIS-LI-DEV <cristianlillocespedes@gmail.com>
2025-07-03 15:49:26 -0400

    actualiza

--- core/settings.py
+++ core/settings.py
@@ -31,10 +31,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+     'corsheaders',
     'app',
 ]
 
 MIDDLEWARE = [
+        'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -115,3 +118,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 ALLOWED_HOSTS = ['*' ]
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "*",
+  
+]
